f6de28e708dd8fd555167844289a556ab602ecf9
salviuez <salviuez19@gmail.com>
2023-01-11 11:22:59 +0530

    AutoAssigning

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 console.log(process.env.MONGO_URL)
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 //connection:
 //const MONGO_URL = "mongodb://127.0.0.1";
